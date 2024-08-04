f366c02d5124f88379d8c9d682f89eec4171e512
AdolfKing <rodrigogfxs@gmail.com>
2024-08-03 21:14:01 -0300

    Footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-const hamburger = document.getElementById("hamburger")
-const menuList = document.getElementById("menulist")
-
+const hamburger = document.getElementById("hamburger");
+const menuList = document.getElementById("menulist");
+const btnMore = document.getElementById("btnmore");
+const readMore = document.getElementById("moretext");
 
 
 hamburger.addEventListener("click", () => {
     menuList.classList.toggle("active")
 })
+
+
+btnMore.addEventListener("click", () => {
+    readMore.classList.toggle("active");
+
+    if (readMore.classList.contains("active")) {
+        btnMore.innerHTML = "Read less";
+    } else {
+        btnMore.innerHTML = "Read more";
+    }
+    console.log(readMore);
+
+});
