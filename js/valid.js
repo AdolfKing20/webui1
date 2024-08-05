d1937c417f9477b550b58d08df2f5338acab7aba
AdolfKing <rodrigogfxs@gmail.com>
2024-08-04 21:09:54 -0300

    Update

--- js/valid.js
+++ js/valid.js
@@ -1,28 +1,48 @@
-
 const form = document.getElementById("formaction");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const validPassword = document.getElementById("validpassword");
 
-const errorUser = document.getElementById("error")
-const errorPass = document.getElementById("errorpass")
-const errorValid = document.getElementById("errorvalid")
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const validPassword = document.getElementById("validpassword").value;
 
-form.addEventListener("submit", e=>{
-    e.preventDefault()
-    errorUser.innerHTML = ""
-    errorPass.innerHTML = ""
-    errorValid.innerHTML = ""
+    const errorUser = document.getElementById("error");
+    const errorPass = document.getElementById("errorpass");
+    const errorValid = document.getElementById("errorvalid");
 
-    if(username.value.length < 5){
-        error.innerHTML = `El nombre de usuario debe tener al menos 5 caracteres`
+    errorUser.innerHTML = "";
+    errorPass.innerHTML = "";
+    errorValid.innerHTML = "";
+
+    const Users = JSON.parse(localStorage.getItem('users')) || [];
+    const isUserRegistered = Users.find(user => user.username === username);
+    
+    let isValid = true;
+
+    if(username.length < 5){
+        errorUser.innerHTML = `El nombre de usuario debe tener al menos 5 caracteres`;
+        isValid = false;
     }
-    if(password.value.length < 8){
-        errorPass.innerHTML = `La contraseña debe tener al menos 8 caracteres`
+    if(password.length < 8){
+        errorPass.innerHTML = `La contraseña debe tener al menos 8 caracteres`;
+        isValid = false;
     }
-    if(validPassword.value === password.value){
-        errorValid.innerHTML = `La contraseña coinciden`
+    if(validPassword !== password){
+        errorValid.innerHTML = `La contraseña no coincide`;
+        isValid = false;
     } else {
-        errorValid.innerHTML = `La contraseña no coincide`
+        errorValid.innerHTML = `Perfecto`;
     }
-})
+
+    if (isUserRegistered) {
+        alert('El usuario ya está registrado!');
+        return;
+    }
+
+    if (isValid) {
+        Users.push({username: username, password: password});
+        localStorage.setItem('users', JSON.stringify(Users));
+        alert('Registro Exitoso!');
+        window.location.href = 'login.html';
+    }
+});
--- js/validlogin.js
+++ js/validlogin.js
@@ -1,21 +1,26 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
 const form = document.getElementById("formaction");
-const errorUser = document.getElementById("error")
-const errorPass = document.getElementById("errorpass")
-const userOk = "adolfking"
-const passOk = "12345678"
+
 
 
 
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
+    const errorUser = document.getElementById("error")
+const errorPass = document.getElementById("errorpass")
+    const username = document.getElementById("username").value
+const password = document.getElementById("password").value
+const Users = JSON.parse(localStorage.getItem(`users`)) || []
+const validUser = Users.find(user => user.username === username && user.password === password)
+if(!validUser){
+    return errorPass.innerHTML = `usuario o contraseña incorrecta`
+}
+alert(`Bienvenido ${validUser.username}`)
+localStorage.setItem('login_success', JSON.stringify(validUser))
+    window.location.href = '../'   
+
+
+
     errorPass.innerHTML = ""
-    if(username.value === userOk && password.value === passOk){
-        alert("acceso exitoso");
-        window.location.href = "../";
-    } else {
-        errorPass.innerHTML = "Verifica que el usuario o contraseña sea correcta"
-    }
+    
 })
